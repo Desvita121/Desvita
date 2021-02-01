d7018f3431a666568ba762e9f578974f090b2af6
Desvita121 <vtulus12@gmail.com>
2021-02-01 11:14:46 +0700

    first commit

---
+++ Desvita121/application/models/m_data.php
@@ -0,0 +1,20 @@
+<?php 
+
+class M_data extends CI_Model{
+
+	public function __construct(){
+                $this->load->database();
+	}
+		
+	function tampil_data(){
+		return $this->db->get('users');
+	}
+
+	function input_data($data,$table){
+		$this->db->insert($table,$data);
+	}
+
+	function edit_data($where,$table){		
+		return $this->db->get_where($table,$where);
+    }
+}
